c65ddc0aa3c06eb5b22e66e6eb1661243e591646
Martin <martin_ma@163.com>
2016-06-24 09:10:42 +0800

    First Add

---
+++ PriceMaintain/PriceMaintain/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PriceMaintain.Startup))]
+namespace PriceMaintain
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
